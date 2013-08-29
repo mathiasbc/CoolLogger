8e0df1bcd0e54c868e5cd9fb6a6d584421f52c15
Mathias Bustamante <mbc@Mathiass-MacBook-Pro.local>
2013-08-29 12:11:04 -0500

    Really simple example showing how to use this module

---
+++ example.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+from logger.logger import Logger
+import traceback
+
+if __name__ == '__main__':
+    logger = Logger(
+        log_filename='log.log',
+        name='test_logger', # Name of the Logger instance, to avoid Singleton
+        keep_zipped = 20    # keep last 20 zipped log files
+    ).getLogger()
+
+    logger.info('Logging INFO line')
+    logger.warning('Warning Logger line')
+    try:
+        len(9)
+        logger.critical('This will fail len(9)')
+    except:
+        logger.exception('Logging ERROR line %s' % traceback.print_exc())
+        pass
+
---
+++ logger/logger.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import logging, logging.handlers
+
+from TimedCompressedRotatingFileHandler import (
+     TimedCompressedRotatingFileHandler
+)
+
+__author__ = "Mathias Bustamante"
+__email__ = "mathiasbc@gmail.com"
+
+
+class Logger(object):
+    ''' Logger class. This class is basedon TimedCompressedRotatingFileHandler 
+    which will create a new file every 24 hours, doing a zip of the last log 
+    file and storing up to N zipped past files. This class also allows to have
+    several logger in the same namespace'''
+
+    def __init__(self, log_filename, name, keep_zipped=50):
+        self.LOG_FILENAME = log_filename
+
+        logging.basicConfig(format='%(asctime)s %(message)s')
+        formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s'
+        )
+        loghandler = TimedCompressedRotatingFileHandler(
+            self.LOG_FILENAME, when="midnight", backupCount=keep_zipped
+        )
+        loghandler.setFormatter(formatter)
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.INFO)
+        self.logger.addHandler(loghandler)
+
+    def getLogger(self):
+        ''' return logger handle '''
+        return self.logger
